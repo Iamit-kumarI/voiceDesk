18584cbb8e8c81a054155b7764a668af5453182c
Iamit-kumarI <amitkumaramitkumar15351@gmail.com>
2026-06-12 15:35:19 +0530

    properties updated need to fix session

--- src/main/java/com/voiceDesk/demo/service/EmailService.java
+++ src/main/java/com/voiceDesk/demo/service/EmailService.java
@@ -8,6 +8,11 @@ import java.util.Properties;
 public class EmailService {
     public void SendEmail(String toEmail){
         Properties prop=new Properties();
-        
+        prop.put("mail.smtp.auth","true");
+        prop.put("mail.smtp.starttls.enable","true");
+        prop.put("mail.smtp.host","smtp.gmail.com");
+        prop.put("mail.smtp.port","587");
+
+        //start a new session
     }
 }
